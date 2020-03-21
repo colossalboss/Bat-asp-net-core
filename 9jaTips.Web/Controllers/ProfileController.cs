a0ef498d8aecea6f1ce844d7b4c14b988d443d11
Ogbara Godwin <ogbaragodwin@Ogbaras-MacBook-Pro.local>
2020-03-21 19:37:57 +0100

    Add Profile functionality

---
+++ 9jaTips.Web/Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _9jaTips.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _9jaTips.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index(Guid id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+
+            if (user != null)
+            {
+                //Instantiate and return ProfileVM
+                return View(user);
+            }
+
+            return NotFound();
+        }
+
+
+
+
+    }
+}
---
+++ 9jaTips.Web/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using _9jaTips.Entities;
+
+namespace _9jaTips.Web.ViewModels
+{
+    public class ProfileViewModel : AppUser
+    {
+        public ProfileViewModel()
+        {
+        }
+
+        
+    }
+}
