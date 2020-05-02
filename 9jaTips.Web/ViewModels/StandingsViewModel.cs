68cb2655951a1567a72101c5783b9947be3f1692
ColossalBoss <ogbaragodwin@Ogbaras-MacBook-Pro.local>
2020-05-02 19:59:58 +0100

    Correct form placeholders and links width

---
+++ 9jaTips.Web/ViewModels/StandingsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace _9jaTips.Web.ViewModels
+{
+    public class StandingsViewModel
+    {
+        public StandingsViewModel()
+        {
+        }
+
+        public string UserName { get; set; }
+    }
+}
